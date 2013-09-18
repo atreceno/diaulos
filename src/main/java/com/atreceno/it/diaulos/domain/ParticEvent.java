510eab9eb270d018cc27d13cba41797b168fece6
Agustin Treceno <agustin.treceno@gmail.com>
2013-09-18 20:28:07 +0100

    Add constrain to ParticEvent entity

--- src/main/java/com/atreceno/it/diaulos/domain/ParticEvent.java
+++ src/main/java/com/atreceno/it/diaulos/domain/ParticEvent.java
@@ -2,7 +2,10 @@ package com.atreceno.it.diaulos.domain;
 
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotNull;
+
 import org.springframework.roo.addon.equals.RooEquals;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.entity.RooJpaEntity;
@@ -14,23 +17,25 @@ import org.springframework.roo.addon.tostring.RooToString;
 @RooJpaEntity
 @RooEquals
 @RooJson
+@Table(uniqueConstraints = { @UniqueConstraint(columnNames = { "event_id",
+		"participant_id" }) })
 public class ParticEvent {
 
-    @NotNull
-    @ManyToOne
-    @JoinColumn(name = "event_id")
-    private Event event;
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name = "event_id")
+	private Event event;
 
-    @NotNull
-    @ManyToOne
-    @JoinColumn(name = "participant_id")
-    private Participant participant;
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name = "participant_id")
+	private Participant participant;
 
-    @ManyToOne
-    @JoinColumn(name = "medal_code")
-    private Medal medal;
+	@ManyToOne
+	@JoinColumn(name = "medal_code")
+	private Medal medal;
 
-    private Integer rank;
+	private Integer rank;
 
-    private String result;
+	private String result;
 }
